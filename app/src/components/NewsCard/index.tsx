ffedaf40784d3ce96b63f90b2429a1eb8da16388
paanSinghCoder <gaurav3017@gmail.com>
2024-02-03 21:36:14 +0530

    fix: img size

--- app/src/components/NewsCard/index.tsx
+++ app/src/components/NewsCard/index.tsx
@@ -4,7 +4,7 @@ import { ENGLISH_NEWS, HINDI_NEWS } from '../../constants/urls'
 
 const NewsCard = () => {
 	return (
-		<div className="border rounded-md w-full h-[590px] my-4">
+		<div className="border rounded-md w-full h-[540px] my-4">
 			<div className="flex justify-between items-center pr-4">
 				<h3 className="py-4 px-4 text-md border-b">Latest News</h3>
 				<div className="">
@@ -25,10 +25,9 @@ const NewsCard = () => {
 				Paytm Erases $2 Billion in Two Days as RBI Action Spooks Market
 			</h3>
 			<p className="text-xs font-light opacity-60 px-4">02 Feb 2024, 10.30AM</p>
-			<p className="opacity-80 text-sm py-4 px-4">
+			<p className="text-sm py-4 px-4">
 				Regulatory troubles for the digital payments giant are mounting after the Reserve Bank of
-				India ordered its unit Paytm Payments Bank Ltd. to stop many of its activities, citing
-				persistent non-compliance and supervisory concerns.
+				India ordered its unit Paytm Payments Bank Ltd.
 			</p>
 		</div>
 	)
