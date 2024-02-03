beb0505402e329fad4bd9fff4d3539db00897feb
paanSinghCoder <gaurav3017@gmail.com>
2024-02-03 21:10:23 +0530

    fix: lighthouse

--- app/src/constants/urls.ts
+++ app/src/constants/urls.ts
@@ -1,5 +1,4 @@
 export const HINDI_NEWS = 'https://www.livehindustan.com/business/story-rbi-may-cancel-permit-of-paytm-payments-bank-how-to-impact-know-here-9276674.html'
 export const ENGLISH_NEWS = 'https://www.livemint.com/market/stock-market-news/rbi-ban-on-paytm-payments-bank-one-97-communications-stock-trades-nearly-80-lower-than-the-ipo-price-11706863311082.html'
-export const NEWS_COVER_IMG = "https://media.assettype.com/bloombergquint%2F2024-02%2Ffbb39772-7dd2-4052-922b-288e7f0504ae%2F_1x_1.jpg?auto=format%2Ccompress&w=720"
 export const ZANSKAR_HOME = 'https://www.zanskar.xyz/'
 export const GITHUB_URL = 'https://github.com/paanSinghCoder/options-analytics'
