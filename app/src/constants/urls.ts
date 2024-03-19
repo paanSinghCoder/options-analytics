f79c4ffd82d7825603c43b1a2fa14e4bfed0f9a0
paanSinghCoder <gaurav3017@gmail.com>
2024-03-19 20:37:33 +0530

    fix: url

--- app/src/constants/urls.ts
+++ app/src/constants/urls.ts
@@ -2,5 +2,5 @@ export const HINDI_NEWS =
 	'https://www.livehindustan.com/business/story-rbi-may-cancel-permit-of-paytm-payments-bank-how-to-impact-know-here-9276674.html'
 export const ENGLISH_NEWS =
 	'https://www.livemint.com/market/stock-market-news/rbi-ban-on-paytm-payments-bank-one-97-communications-stock-trades-nearly-80-lower-than-the-ipo-price-11706863311082.html'
-export const TRADEX_HOME = 'https://tradex.vercel.app/'
+export const TRADEX_HOME = 'https://tradex-options.vercel.app/'
 export const GITHUB_URL = 'https://github.com/paanSinghCoder/options-analytics'
